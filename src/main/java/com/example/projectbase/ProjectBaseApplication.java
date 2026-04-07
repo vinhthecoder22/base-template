8e3f13b1680f3ddfc479c987e27d19145b0a2fc8
Thevinh8386 <nguyenthevinh02022005@gmail.com>
2026-04-07 21:33:11 +0700

    chore: initialize project base

---
+++ src/main/java/com/example/projectbase/ProjectBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBaseApplication.class, args);
+    }
+
+}
